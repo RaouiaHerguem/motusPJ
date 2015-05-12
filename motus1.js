b0ef54bbb776efa13bf01577a29deeaeddf2cce7
lobna2015 <lobnadrissi2020@gmail.com>
2015-05-12 22:48:40 +0200

    Create motus1.js

---
+++ motus1.js
@@ -0,0 +1,185 @@
+var app=angular.module("myMotus",[])
+
+app.controller("motusController",function($scope){
+
+$scope.Alphabet={
+		 {car:'a',img1:'a1.jpg',img2:'a2.jpg',img3:'a3.jpg'},
+		 {car:'b',img:'b.jpg'},
+		 {car:'c',img:'c.jpg'},
+		 {car:'d',img:'d.jpg'},
+		 {car:'e',img:'e.jpg'},
+		 {car:'f',img:'f.jpg'},
+		 {car:'g',img:'g.jpg'},
+		 {car:'h',img:'h.jpg'},
+		 {car:'i',img:'j.jpg'},
+		 {car:'k',img:'k.jpg'},
+		 {car:'l',img:'l.jpg'},
+		 {car:'m',img:'m.jpg'},
+		 {car:'n',img:'n.jpg'},
+		 {car:'o',img:'o.jpg'},
+		 {car:'p',img:'p.jpg'},
+		 {car:'q',img:'q.jpg'},
+		 {car:'r',img:'r.jpg'},
+		 {car:'s',img:'s.jpg'},
+		 {car:'t',img:'t.jpg'},
+		 {car:'u',img:'u.jpg'},
+		 {car:'v',img:'v.jpg'},
+		 {car:'w',img:'w.jpg'},
+		 {car:'x',img:'x.jpg'},
+		 {car:'y',img:'y.jpg'},
+		 {car:'z',img:'z.jpg'}
+		};
+		
+		$scope.motsCache={stylites, aluminât, cémenter, crispent, déplante, cariacou, vocatifs, fécondée, poulinez,
+		agnelins, arçonnés, inclinai, fricotez, rassurée, comblons, scorbuts, remparai, avérions, pâterons, sacquées,
+		 };
+		 
+		 $scope.motSelet=[now.getSeconds() % $scope.motsCache.length];
+		
+        $scope.matrix={
+		 {
+			{col:1,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:1,car:'x',img'Vide.jpg',etat:0},
+			{col:3,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:1,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:1,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:2,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:2,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:3,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:3,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:4,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:4,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:5,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:5,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:6,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:6,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		 {
+			{col:1,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:2,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:3,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:4,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:5,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:6,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:7,row:7,car:'x',img:'Vide.jpg',etat:0},
+			{col:8,row:7,car:'x',img:'Vide.jpg',etat:0}
+		 }
+		}
+	
+	
+
+  $scope.go = function()
+{
+	var j=0;
+     var  i=0;
+   $scope.motEcris = $("#txt");
+
+if($scope.motEcris.val() != " ")
+
+if(i<8){
+$scope.matrix[i][0].car=$scope.motEcris[0]-'A';
+$scope.matrix[i][1].car=$scope.motEcris[1]-'A';
+$scope.matrix[i][2].car=$scope.motEcris[2]-'A';
+$scope.matrix[i][3].car=$scope.motEcris[3]-'A';
+$scope.matrix[i][4].car=$scope.motEcris[4]-'A';
+$scope.matrix[i][5].car=$scope.motEcris[5]-'A';
+$scope.matrix[i][6].car=$scope.motEcris[6]-'A';
+$scope.matrix[i][7].car=$scope.motEcris[7]-'A';
+		
+		i++;
+	
+}}
+
+$scope.Initial=function(){
+var i=0;
+var k=0;
+var j=0;
+if(k<26){
+if(i<8){
+if(j<8){
+{if(matrix[i][j].car==Alphabet[k].car)
+{matrix[i][j].img=Alphabet[k].img}
+
+}
+i++;
+j++;
+k++;  }
+      }
+       }
+                      }
+					  
+$scope.Recherche=function(char x,var mot){
+for(var h=0;h<mot.length;h++){
+if(mot[h]==x){return(true)}
+else{return(false)}
+}
+}
+
+
+$scope.Test=function(){
+var j=0;
+var i=0;
+if((i<8)&&(j<8)){
+if(matrix[i][j].car==motSelet[j]){
+
+var t=0;
+
+for(var d=0;d<26;d++){if(Alphabet[d].car==matrix[i][j].car){ t=k;}}
+matrix[i][j].img=Alphabet[t].img1;}
+ 
+else if(Recherche(matrix[i][j].car,motSelect)==true){
+for(var d=0;d<26;d++){if(Alphabet[d].car==matrix[i][j].car){ t=k;}}
+matrix[i][j].img=Alphabet[t].img2;}
+
+else 
+matrix[i][j].img=Alphabet[t].img3;
+
+i++;
+j++;}
+}					  
+
+}
+);
